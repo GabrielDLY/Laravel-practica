d0c126e792e678779ba0105400b900b97a5c18ed
GabrielDLY <negriarroyo7@gmail.com>
2023-05-31 20:28:58 -0300

    templates

---
+++ resources/views/post.blade.php
@@ -0,0 +1,9 @@
+@extends('template')
+
+@section('content')
+    
+    <h1>Detalles</h1>
+
+    {{ $post }}
+    
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/**
+ * Route::get    |Consultar
+ * Route::post   |Guardar
+ * Route::delete |Eliminar
+ * Route::put    |Actualizar
+ */
+
+Route::get('/', function () {
+    return view('home');
+})->name('home');
+
+Route::get('blog', function () {
+    //Consulta de base de datos
+
+    $posts = [
+        ['id' => 1, 'title' => 'PHP', 'slug' => 'php'],
+        ['id' => 2, 'title' => 'Laravel', 'slug' => 'laravel']
+    ];
+    return view('blog', ['posts' => $posts]);
+})->name('blog');
+
+Route::get('blog/{slug}', function ($slug) {
+    //Consulta de base de datos
+    $post = $slug;
+    return view('post', ['post' => $post]);
+})->name('post');
+
+// Route::get('buscar', function (Request $request) {
+//     //Consulta de base de datos
+//     return $request->all();
+// })->name('buscar');
+
