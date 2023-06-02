4195ad25aa35e7ac713b8303bea3f1ca3a370603
GabrielDLY <negriarroyo7@gmail.com>
2023-06-01 21:26:58 -0300

    crear-y-editar-primer-paso

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,24 +1,30 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
 class PostController extends Controller
 {
-   public function index()
-   {
-      return view('posts.index', [
-         'posts' => Post::latest()->paginate()
-      ]);
-   }
+    public function index() 
+    {
+    	return view('posts.index', [
+            'posts' => Post::latest()->paginate()
+        ]);
+    }
 
-   public function destroy(Post $post)
-   {
-      $post->delete();
-      
-      return back();
-   }
+    public function create() 
+    {
+        return view('posts.create');
+    }
+
+    public function edit(Post $post) 
+    {
+        return view('posts.edit', compact('post'));
+    }
+
+    public function destroy(Post $post) 
+    {
+        $post->delete();
+        return back();
+    }
 }
