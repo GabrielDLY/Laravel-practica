9b8b832fdf3ab39a9168a665474b7f54dc8684a4
GabrielDLY <negriarroyo7@gmail.com>
2023-06-03 14:49:07 -0300

    destacados

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,12 @@
 @extends('template')
 
-@section('content')
-<div>
-    <!-- Destacado -->
+@section('content') 
+<div class="bg-gray-900 px-20 py-16 rounded-lg mb-8 relative overflow-hidden">
+    <span class="text-xs uppercase text-gray-700 bg-gray-400 rounded-full px-2 py-1">Programacion</span>
+    <h1 class=" text-3xl text-white mt-4" >Blog</h1>
+    <p class="text-sm text-gray-400 mt-2">Desarrollo web</p>
+
+    <img src="{{ asset('images/dev.png') }}" class=" absolute -right-20 -bottom-20 opacity-50">
 </div>
 
 <div class="px-4">
